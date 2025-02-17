0f553bf2646f167a95eb069bd4368830b3a66a40
Clark Kampfe <clark.kampfe@gmail.com>
2025-02-16 19:59:20 -0600

    move htmx ticker js to own file

---
+++ src/htmx_ticker.js
@@ -0,0 +1,55 @@
+const filledBlock = '\u{2588}';
+const emptyBlock = '\u{3000}';
+
+var shouldAddCursor = false;
+var timer;
+
+function swap(id, a, b) {
+    let el = document.getElementById(id);
+    el.innerHTML = el.innerHTML.replace(a, b);
+}
+
+function removeAllBlocks(id) {
+    swap(id, emptyBlock, '');
+    swap(id, filledBlock, '');
+}
+
+function alternateCursor() {
+    if (shouldAddCursor) {
+        swap('llm-response', emptyBlock, filledBlock);
+        shouldAddCursor = false;
+    } else {
+        swap('llm-response', filledBlock, emptyBlock);
+        shouldAddCursor = true;
+    }
+}
+
+function startTimer() {
+    timer = setInterval(alternateCursor, 1000);
+}
+
+function stopTimer() {
+    clearInterval(timer);
+    timer = null;
+}
+
+document.addEventListener('htmx:sseBeforeMessage', function (e) {
+    if (timer) {
+        stopTimer();
+    }
+    removeAllBlocks('llm-response');
+});
+
+document.addEventListener('htmx:sseClose', function (e) {
+    stopTimer();
+    removeAllBlocks('llm-response');
+    const responseEl = document.getElementById('llm-response');
+    responseEl.removeAttribute('id');
+});
+
+document.addEventListener('htmx:afterRequest', function (e) {
+    // TODO base this on an id instead
+    if (e.srcElement.tagName === 'FORM') {
+        startTimer();
+    }
+});
